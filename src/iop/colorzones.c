dcc47f5dc1bd6d8db34bf9a20509a56da22214c7
Dan Torop <dant@pnym.net>
2022-02-08 21:53:02 -0500

    pickers: hack to hide picker when iop is no longer active
    As the picker data isn't updated once the iop is active, de-select it.
    
    Question: What was the rationale for keeping these pickers active?
    Question: The blend picker also is "keep-active" -- do we want this?

--- src/iop/colorzones.c
+++ src/iop/colorzones.c
@@ -2451,7 +2451,7 @@ void gui_init(struct dt_iop_module_t *self)
   // color pickers
   c->colorpicker = dt_color_picker_new_with_cst(self, DT_COLOR_PICKER_POINT_AREA, hbox, iop_cs_LCh);
   gtk_widget_set_tooltip_text(c->colorpicker, _("pick GUI color from image\nctrl+click or right-click to select an area"));
-  gtk_widget_set_name(c->colorpicker, "keep-active");
+  //gtk_widget_set_name(c->colorpicker, "keep-active");
   c->colorpicker_set_values = dt_color_picker_new_with_cst(self, DT_COLOR_PICKER_AREA, hbox, iop_cs_LCh);
   dtgtk_togglebutton_set_paint(DTGTK_TOGGLEBUTTON(c->colorpicker_set_values),
                                dtgtk_cairo_paint_colorpicker_set_values,
